50cf2557eef072dd4ec9d15c214c4acf709e65ac
emanherawy <eman.herawy@gmail.com>
2020-09-17 17:48:18 +0200

    update metacoin contract

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -3,32 +3,43 @@ pragma solidity ^0.6.2;
 
 import "./ConvertLib.sol";
 import "@opengsn/gsn/contracts/BaseRelayRecipient.sol";
+import "@opengsn/gsn/contracts/interfaces/IKnowForwarderAddress.sol";
+
 
 // This is just a simple example of a coin-like contract.
 // It is not standards compatible and cannot be expected to talk to other
 // coin/token contracts. If you want to create a standards-compliant
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
-contract MetaCoin is BaseRelayRecipient {
-
+contract MetaCoin is BaseRelayRecipient, IKnowForwarderAddress {
     string public symbol = "META";
     string public description = "GSN Sample MetaCoin";
-    uint public decimals = 0;
+    uint256 public decimals = 0;
 
-    mapping(address => uint) balances;
+    mapping(address => uint256) balances;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    constructor(address forwarder) public {
+    constructor() public {
         balances[tx.origin] = 10000;
-        trustedForwarder = forwarder;
+    }
+
+    function getTrustedForwarder() public override view returns (address) {
+        return trustedForwarder;
+    }
+
+    function setTrustedForwarder(address _trustedForwarder) public {
+        trustedForwarder = _trustedForwarder;
     }
 
     function versionRecipient() external override view returns (string memory) {
         return "2.0.0";
     }
 
-    function transfer(address receiver, uint amount) public returns (bool sufficient) {
+    function transfer(address receiver, uint256 amount)
+        public
+        returns (bool sufficient)
+    {
         if (balances[_msgSender()] < amount) return false;
         balances[_msgSender()] -= amount;
         balances[receiver] += amount;
@@ -36,14 +47,13 @@ contract MetaCoin is BaseRelayRecipient {
         return true;
     }
 
-    function getBalanceInEth(address addr) public view returns (uint){
+    function getBalanceInEth(address addr) public view returns (uint256) {
         return ConvertLib.convert(balanceOf(addr), 2);
     }
 
-    function balanceOf(address addr) public view returns (uint) {
+    function balanceOf(address addr) public view returns (uint256) {
         return balances[addr];
     }
-
 
     mapping(address => bool) minted;
 

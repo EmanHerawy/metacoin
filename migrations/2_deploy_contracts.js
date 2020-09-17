43fe4ab0e57037e4f79ec041ff33e5854f69d4d0
emanherawy <eman.herawy@gmail.com>
2020-09-17 17:50:48 +0200

    update migration to for local and testnet usage

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,27 +1,69 @@
-const MetaCoin = artifacts.require('MetaCoin.sol')
-const TrustedForwarder = artifacts.require('Forwarder.sol')
+const MetaCoin = artifacts.require('MetaCoin')
+const NaivePaymaster = artifacts.require('NaivePaymaster.sol')
 
-module.exports = async function deployFunc (deployer, network) {
-  const netid = await web3.eth.net.getId()
+const {
+  networks
+} = require('../app/scripts/networks')
+// console.log( Object.keys(networks),'networks');
+console.log(networks[3].forwarder, 'networks');
+console.log(networks["3"]["forwarder"], 'networks[][]');
 
-  // first, check if already deployed through truffle:
-  let forwarder = await TrustedForwarder.deployed().then(c => c.address).catch(e => null)
-  if (!forwarder) {
-    if (netid > 100) {
-      forwarder = (await deployer.deploy(TrustedForwarder)).address
+module.exports = async function (deployer, network) {
+  await deployer.deploy(NaivePaymaster)
+  const paymaster = await NaivePaymaster.deployed()
+
+  console.log(paymaster, 'paymaster');
+
+  await deployer.deploy(MetaCoin)
+  const forwarderAddress = getForwarder(network)
+  const relayAddress = getRelayHub(network)
+  // const tx=  await paymaster.send('1e18')
+
+  await paymaster.setRelayHub(relayAddress)
+
+  console.log(forwarderAddress, 'forwarderAddress');
+
+  const instance = await MetaCoin.deployed()
+  await paymaster.setTarget(instance.address)
+  const accounts = await web3.eth.getAccounts()
+  web3.eth.sendTransaction({
+    from: accounts[0],
+    to: paymaster.address,
+    value: 2e18
+  }, (e, r) => {
+    if (e) {
+      console.log('Failed to fund metamask', e)
     } else {
-      // don't automatically install on other networks. should have entires here.
-      const forwarders = {
-        42: '0x663946D7Ea17FEd07BF1420559F9FB73d85B5B03',
-        3: '0x766400B526fB5889AE6C52b369671F5eE137880b'
-      }
-      forwarder = forwarders[netid]
+      console.log('Funded metamask @', paymaster.address)
     }
-    if (!forwarder) {
-      throw new Error('no valid forwarder for network ', network)
-    }
+  })
+  await paymaster.setTrustedForwarder(forwarderAddress)
+  await instance.setTrustedForwarder(forwarderAddress)
+  console.log(`Successfully set Trusted Forwarder (${forwarderAddress}) on Recipient (${instance.address})`)
+}
+
+function getForwarder (network) {
+  console.log(network, 'network name');
+  if (network === 'development') {
+    return require('../build/gsn/Forwarder.json').address
+  } else if (network === 'ropsten' || network === 'ropsten-fork') {
+    return networks[3].forwarder;
+  } else if (network === 'kovan') {
+    return networks['42'].forwarder;
+  } else {
+    return null
   }
-  console.log('using forwarder: ', forwarder)
-  await deployer.deploy(MetaCoin, forwarder)
-  console.log('Finished 2/3 migrations files')
+}
+
+function getRelayHub (network) {
+  console.log(network, 'network name');
+  if (network === 'development') {
+    return require('../build/gsn/RelayHub.json').address
+  } else if (network === 'ropsten' || network === 'ropsten-fork') {
+    return networks[3].forwarder;
+  } else if (network === 'kovan') {
+    return networks['42'].forwarder;
+  } else {
+    return null
+  }
 }
